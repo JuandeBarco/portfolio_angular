c259d767da6d435dcbbc76ec6896eba1a1a1934d
JuandeBarco <lextadz@gmail.com>
2025-02-19 11:35:00 -0600

    Update app component with personalized developer name and new portfolio message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  dev_name = 'John Doe';
+  dev_name = 'Juan de Dios Barajas';
 }
